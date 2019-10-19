f33e75ccfc89ebe69d902ca56968ea526d4c14a3
Patrick Longo <ck.longo@bol.com.br>
2019-10-19 11:58:16 -0300

    First Commit

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const Product = mongoose.model('Product')
+
+module.exports = {
+    async index(req, res){
+        const {page = 1} = req.query
+        const products = await Product.paginate({}, {page, limit: 10}) // Paginate substitui o Product.find() para retorno moderado de resultados
+        return res.json(products)
+    },
+
+    async show(req, res){
+        const product = await Product.findById(req.params.id)
+        return res.json(product)
+    },
+
+    async store(req, res){
+        const product = await Product.create(req.body)
+
+        return res.json(product)
+    },
+
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true}) // Retorna o produto ja atualizado para variavel
+        
+        return res.json(product)
+    },
+
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id)
+
+        res.send('Deletado com sucesso.')
+    },
+}
